b122d6f12d92b90b2fe677697a13b658bbd33673
P4u1wr3 <38630889+P4u1wr3@users.noreply.github.com>
2018-04-22 19:52:26 +0200

    Abstrakte Klasse Tier + Interface(beides unfertig)

---
+++ Vogel/src/Zoo/ITier.java
@@ -0,0 +1,9 @@
+package Zoo;
+
+public interface ITier     //Änderung des Wertes der Variablen Art, Name etc
+{
+	String setArt(); 
+	String setName(); 
+	String setGeschlecht(); 
+	String setLebensraum();
+}
---
+++ Vogel/src/Zoo/Tier.java
@@ -0,0 +1,15 @@
+package Zoo;
+
+public abstract class Tier implements ITier
+{ 
+	String Art; 
+	String Name; 
+	String Geschlecht; 
+	String Lebensraum;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
