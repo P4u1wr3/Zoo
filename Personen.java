5095aaf89b95bebd9016f22e94c4fe88ff971ac7
AlnVlkn <alena8180@gmail.com>
2018-04-25 17:15:27 +0200

    Add files via upload
    Class Personen

---
+++ Personen.java
@@ -0,0 +1,14 @@
+
+public abstract class Personen {
+	private boolean status;
+	protected int gehege;
+	
+	
+	public boolean istMitarbeiter() {
+		return status;
+	}
+	
+	public int gehege() {
+		return gehege;
+	}
+}
